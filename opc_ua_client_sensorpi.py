7c1b660bbd784b260fb9f03d6fcb7854f30e6468
aniederm <armin.niedermueller@salzburgresearch.at>
2018-08-22 17:01:19 +0200

    ...

--- opc_ua_client_sensorpi.py
+++ opc_ua_client_sensorpi.py
@@ -24,11 +24,9 @@ if __name__ == '__main__':
     with HelloClient("opc.tcp://192.168.48.42:4840/freeopcua/server/") as client:
         root = client.get_root_node()
         print("Root node is: ", root)
-        objects = client.get_objects_node()
-        print("Objects node is: ", objects)
 
-        belt_mover = objects.get_child("0:belt_mover")
-        print("belt_mober is: ", belt_mover)
+        methods = client.get_objects_node("Methods")
+        print("Methods node is: ", methods)
 
-        resulting_text = hellower.call_method("0:belt_mover", 10, True)
-        print(resulting_text)
+        result = belt_mover.call_method("move_belt", [True, 10])
+        print(result)
