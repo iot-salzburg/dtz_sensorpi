0ab54497de06a0dc600f1237d1ef6928d87f0b67
aniederm <41986362+aniederm@users.noreply.github.com>
2018-08-24 11:27:51 +0200

    functions working - but belt is still moving endlessly...

--- opc_ua_client_sensorpi.py
+++ opc_ua_client_sensorpi.py
@@ -1,35 +1,48 @@
-# Salzburg Research ForschungsgesmbH
-# Armin Niedermueller
+import time
+import sys
+sys.path.insert(0, "..")
 
-# OPC UA Client on Sensor Pi
-
-from opcua import Client, ua
-from opcua.ua import ua_binary as uabin
-from opcua.common.methods import call_method
+from multiprocessing import Process
+from opcua import Client
 
 
-class HelloClient:
-    def __init__(self, endpoint):
-        self.client = Client(endpoint)
+if __name__ == "__main__":
 
-    def __enter__(self):
-        self.client.connect()
-        return self.client
+    client = Client("opc.tcp://192.168.48.42:4840/freeopcua/server/")
+    # client = Client("opc.tcp://admin@localhost:4840/freeopcua/server/") #connect using a user
+    try:
+        client.connect()
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.client.disconnect()
-
-
-if __name__ == '__main__':
-    with HelloClient("opc.tcp://192.168.48.42:4840/freeopcua/server/") as client:
+        # Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
         root = client.get_root_node()
-        print("Root node is: ", root)
+        print("Objects node is: ", root)
 
-        objects = client.get_objects_node()
-        print("Objects node is: ", objects)    
+        # Node objects have methods to read and write node attributes as well as browse or populate address space
+        print("Children of root are: ", root.get_children())
 
-        mover = objects.get_child("0:Mover")
-        print("Mover is: ", mover)
+        # get a specific node knowing its node id
+        #var = client.get_node(ua.NodeId(1002, 2))
+        #var = client.get_node("ns=3;i=2002")
+        #print(var)
+        #var.get_data_value() # get value of node as a DataValue object
+        #var.get_value() # get value of node as a python builtin
+        #var.set_value(ua.Variant([23], ua.VariantType.Int64)) #set node value using explicit data type
+        #var.set_value(3.9) # set node value using implicit data type
 
-        result = mover.call_method("0:Move", True)
-        print(result)
+        # Now getting a variable node using its browse path
+        server_time = root.get_child(["0:Objects", "2:Object1", "2:Time"])
+        object1 = root.get_child(["0:Objects", "2:Object1"])
+        mover = root.get_child(["0:Objects", "2:Object1", "2:MoveBelt"])
+        #object1 = root.get_child(["0:Objects", "2:MyObject"])
+        object1.call_method("2:MoveBelt", True)
+        print("first call")
+
+        print("time is: ", server_time)
+        print("object1 is: ", object1)
+
+        time.sleep(14)
+
+        object1.call_method("2:MoveBelt", False)
+        print("second call")
+    finally:
+        client.disconnect()
